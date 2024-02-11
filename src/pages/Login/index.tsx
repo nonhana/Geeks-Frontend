d338713462fc67dc36308a612393449a72269ad3
non_hana <1209220829@qq.com>
2024-02-11 18:16:24 +0800

    feat: 完成登录功能

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,49 @@
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from "@/assets/imgs/logo.png";
+
 const Login = () => {
-  return <div>this is Login</div>;
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form onFinish={onFinish} validateTrigger="onBlur">
+          <Form.Item
+            name="mobile"
+            // 多条校验逻辑
+            // 从上到下依次校验，只要有一条校验不通过，后面的校验就不再执行
+            rules={[
+              {
+                required: true,
+                message: "请输入手机号",
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "手机号格式不正确",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "请输入密码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
