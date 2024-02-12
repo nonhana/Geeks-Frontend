c4e0b202c97bd39ba3a9447730bb520f7ad0790e
non_hana <1209220829@qq.com>
2024-02-12 11:48:22 +0800

    feat: 封装高阶组件控制无token时候的路由跳转

---
+++ src/components/AuthRoute.tsx
@@ -0,0 +1,15 @@
+// src/components/AuthRoute.tsx
+// 高阶组件：有token，正常渲染组件，否则跳转到登录页
+import { getToken } from "@/utils";
+import { Navigate } from "react-router-dom";
+
+function AuthRoute(props: { children: React.ReactNode }) {
+  const token = getToken();
+  if (token) {
+    return props.children;
+  } else {
+    return <Navigate to={"/login"} replace />;
+  }
+}
+
+export default AuthRoute;
