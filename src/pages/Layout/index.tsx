267ee859a60f1ca0ec60efac03e82939e3da0205
non_hana <1209220829@qq.com>
2024-02-12 15:45:03 +0800

    feat: 二级路由配置

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -6,6 +6,7 @@ import {
   LogoutOutlined,
 } from "@ant-design/icons";
 import "./index.scss";
+import { Outlet } from "react-router-dom";
 
 const { Header, Sider } = Layout;
 
@@ -52,7 +53,8 @@ const GeekLayout = () => {
           ></Menu>
         </Sider>
         <Layout className="layout-content" style={{ padding: 20 }}>
-          内容
+          {/* 二级路由的出口位置 */}
+          <Outlet />
         </Layout>
       </Layout>
     </Layout>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,6 +5,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
 import AuthRoute from "@/components/AuthRoute";
+import Home from "@/pages/Home";
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish";
 
 // 配置路由实例
 const router = createBrowserRouter([
@@ -15,6 +18,20 @@ const router = createBrowserRouter([
         <Layout />
       </AuthRoute>
     ),
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/article",
+        element: <Article />,
+      },
+      {
+        path: "/publish",
+        element: <Publish />,
+      },
+    ],
   },
   {
     path: "/login",
